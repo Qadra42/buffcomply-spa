18aceee4283ee4cbc7f7c3393ddf7c89e60983d0
Francisco Cuadra <francisco@mimiquate.com>
2025-03-02 18:13:10 -0300

    chorE: ignore during build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
   },
+  eslint: {
+    // Desactivar la verificación de ESLint durante la compilación
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig 
